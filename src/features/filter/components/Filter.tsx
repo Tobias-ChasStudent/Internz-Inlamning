12d0b2d7badbbe2d3b849379c1495fb0a2da6202
Hampus Dufberg Töttrup <Hampus>
2023-03-15 08:27:46 +0100

    wip basic layout done

--- src/features/filter/components/Filter.tsx
+++ src/features/filter/components/Filter.tsx
@@ -1,25 +1,46 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import useToggle from "../../../hooks/useToggle";
-import FilterItem from "./FilterItem";
+import FilterItems from "./FilterItems";
+
+const handleClearAllFilters = () => {
+  console.log("Clearing all filters");
+};
 
 const Filter = () => {
   const [active, toggleActive] = useToggle();
 
-  const placeholderArray: Array<any> = [];
+  const placeholderArray: Array<any> = [
+    { name: "category 1", items: [{ asd: 123 }, { asd: 123 }, { asd: 123 }] },
+    { name: "category 1", items: [{ asd: 123 }, { asd: 123 }, { asd: 123 }] },
+  ];
 
   type Props = {
     key: number;
-    item: object;
+    cat: object;
+    index: boolean;
   };
 
   return (
     <div className="rounded-xl bg-primary p-4">
       <SearchBar toggleActive={toggleActive} />
-      {active &&
-        placeholderArray.map((item, index) => (
-          <FilterItem key={index} item={item} />
-        ))}
+
+      {active && (
+        <>
+          <div className="my-2 flex justify-between">
+            <div>
+              Filter{" "}
+              <span className="rounded-lg bg-gray-200 p-1">{"amount"}</span>
+            </div>
+            <button className="text-[#5fa3e1]" onClick={handleClearAllFilters}>
+              Clear all
+            </button>
+          </div>
+          {placeholderArray.map((cat, index) => (
+            <FilterItems key={index} />
+          ))}
+        </>
+      )}
     </div>
   );
 };
