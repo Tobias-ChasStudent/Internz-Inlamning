107780592ef9700a34a87d635f96beca543b180b
Hampus Dufberg Töttrup <Hampus>
2023-03-15 13:05:38 +0100

    basic config of RTK

---
+++ src/store/index.ts
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "../features/user/userSlice";
+import finderReducer from "../features/finder/finderSlice";
+
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+    finder: finderReducer,
+  },
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
