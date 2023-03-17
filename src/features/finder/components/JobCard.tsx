ba16afc9b15e25eff6c84f450972b394f3a078c7
Tobias-ChasStudent <tobias.riiser@chasacademy.se>
2023-03-17 14:12:23 +0100

    Added typescript types and margin to cards

--- src/features/finder/components/JobCard.tsx
+++ src/features/finder/components/JobCard.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
+import JobTag from "./JobTag";
+
+type CardType = {
+    companyName: string
+    companyIcon: string
+    title: string
+    desc: string
+    tags: Array<string>
+}
+
+const cards: Array<CardType> = [
+    {
+        companyName: "Google",
+        companyIcon: "google.png",
+        title: "Full Stack Developer",
+        desc: "In this position you will be working with a senior FS developer",
+        tags: ["Javascript", "React", "Firebase", "Chakra UI"],
+    },
+    {
+        companyName: "Google",
+        companyIcon: "google.png",
+        title: "Full Stack Developer",
+        desc: "In this position you will be working with a senior FS developer",
+        tags: ["Javascript", "React", "Firebase", "Chakra UI"],
+    },
+    {
+        companyName: "Google",
+        companyIcon: "google.png",
+        title: "Full Stack Developer",
+        desc: "In this position you will be working with a senior FS developer",
+        tags: ["Javascript", "React", "Firebase", "Chakra UI"],
+    },
+]
 
 const JobCard = () => {
 
-    const cards = [
-        {
-            companyName: "Google",
-            companyIcon: "google.png",
-            title: "Full Stack Developer",
-            desc: "In this position you will be working with a senior FS developer",
-            tags: ["Javascript", "React", "Firebase", "Chakra UI"],
-        },
-    ]
 
     return (
         <div>
 
             {
                 /* Logo, Company name, Job name, Tags, Description */
-                cards.map((card) => {
+                cards.map((card: CardType) => {
 
                     return (
-                        <div key={cards.indexOf(card)} className='bg-white p-3 rounded-lg m-auto drop-shadow-md'>
+                        <div key={cards.indexOf(card)} className='bg-white p-3 rounded-lg m-auto mb-[20px] drop-shadow-md'>
                             <div className="card-header flex">
                                 <img className='card-logo w-10' src={card.companyIcon} alt={card.companyName + " logo"} />
                                 {card.companyName}
@@ -32,7 +56,10 @@ const JobCard = () => {
                                 {
                                     card.tags.map((tag) => {
                                         return (
-                                            <li key={card.tags.indexOf(tag)} className='mr-4 bg-background p-1 rounded-[10px]'>{tag}</li>
+                                            <JobTag
+                                            tag={tag}
+                                            key={card.tags.indexOf(tag)}
+                                            />
                                         )
                                     })
                                 }
