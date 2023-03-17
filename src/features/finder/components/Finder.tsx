ae79eeb84e82427856b9e68a19dfdec7e9b88da5
Tobias-ChasStudent <tobias.riiser@chasacademy.se>
2023-03-17 09:50:27 +0100

    Made a card design with dumb data

--- src/features/finder/components/Finder.tsx
+++ src/features/finder/components/Finder.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import JobCard from "./JobCard";
 
 const Finder = () => {
-  return <div>Finder</div>;
+  return <div>
+    Finder
+    <JobCard/>
+  </div>;
 };
 
 export default Finder;
--- src/features/finder/components/JobCard.tsx
+++ src/features/finder/components/JobCard.tsx
@@ -1,9 +1,48 @@
 import React from 'react';
 
 const JobCard = () => {
+
+    const cards = [
+        {
+            companyName: "Google",
+            companyIcon: "google.png",
+            title: "Full Stack Developer",
+            desc: "In this position you will be working with a senior FS developer",
+            tags: ["Javascript", "React", "Firebase", "Chakra UI"],
+        },
+    ]
+
     return (
         <div>
-            
+
+            {
+                /* Logo, Company name, Job name, Tags, Description */
+                cards.map((card) => {
+
+                    return (
+                        <div key={cards.indexOf(card)} className='bg-white p-3 rounded-lg m-auto drop-shadow-md'>
+                            <div className="card-header flex">
+                                <img className='card-logo w-10' src={card.companyIcon} alt={card.companyName + " logo"} />
+                                {card.companyName}
+                            </div>
+                            <div className="card-desc">
+                                {card.desc}
+                            </div>
+                            <ul className="card-tags flex">
+                                {
+                                    card.tags.map((tag) => {
+                                        return (
+                                            <li key={card.tags.indexOf(tag)} className='mr-4 bg-background p-1 rounded-[10px]'>{tag}</li>
+                                        )
+                                    })
+                                }
+                            </ul>
+                        </div>
+                    )
+                })
+
+            }
+
         </div>
     );
 };
