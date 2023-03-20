357be994e2bcfceb0120f437d0a9605f3040f05d
sndell <sundell.benjamin@gmail.com>
2023-03-20 16:18:59 +0100

    feat: changed routing &  link to create new job

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./index.css";
 import { Finder } from "./features/finder";
 import { Provider } from "react-redux";
 import store from "./store/index";
+import New from "./features/jobs/components/New";
 
 const router = createBrowserRouter([
   {
@@ -17,8 +18,8 @@ const router = createBrowserRouter([
         element: <Finder />,
       },
       {
-        path: "/test",
-        element: <Finder />,
+        path: "/company/new",
+        element: <New />,
       },
     ],
   },
