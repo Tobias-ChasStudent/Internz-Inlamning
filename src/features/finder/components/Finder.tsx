39b3c109c6ce11c6ad3a40afad4be8f5f47e8462
sndell <sundell.benjamin@gmail.com>
2023-03-20 13:34:38 +0100

    test

--- src/features/finder/components/Finder.tsx
+++ src/features/finder/components/Finder.tsx
@@ -2,10 +2,25 @@ import React from "react";
 import JobCard from "./JobCard";
 
 const Finder = () => {
-  return <div>
-    Finder
-    <JobCard/>
-  </div>;
+  return (
+    <div>
+      Finder
+      <JobCard />
+    </div>
+  );
+};
+
+export default Finder;
+
+import { Filter } from "../../filter/index";
+
+const Finder = () => {
+  return (
+    <div>
+      Finder
+      <Filter />
+    </div>
+  );
 };
 
 export default Finder;
